ef5ff0e898e5f0eeee2d26211db957a5d1d1aacf
louispro12 <louispro333@gmail.com>
2025-08-14 13:40:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+﻿// Simple fade-in animation when page loads
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.style.opacity = 1;
+});
+
+// Mobile menu toggle (optional future feature)
+
